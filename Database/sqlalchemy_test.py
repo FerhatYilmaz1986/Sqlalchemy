097a487ba41f5d0d1355e4bbcf4fca7fc90eff76
Ferhat <47542683+FerhatYilmaz1986@users.noreply.github.com>
2019-12-06 11:26:35 -0500

    Feature : Added dispatch_order

--- Database/sqlalchemy_test.py
+++ Database/sqlalchemy_test.py
@@ -67,3 +67,28 @@ class Customer(Base):
     created_on = Column(DateTime(), default=datetime.now)
     updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
     orders = relationship("Order", backref='customer')
+    
+def dispatch_order(order_id):
+    # check whether order_id is valid or not
+    order = session.query(Order).get(order_id)
+
+    if not order:
+        raise ValueError("Invalid order id: {}.".format(order_id))
+
+    if order.date_shipped:
+        print("Order already shipped.")
+        return
+
+    try:
+        for i in order.order_lines:
+            i.item.quantity = i.item.quantity - i.quantity
+
+        order.date_shipped = datetime.now()
+        session.commit()
+        print("Transaction completed.")
+
+    except IntegrityError as e:
+        print(e)
+        print("Rolling back ...")
+        session.rollback()
+        print("Transaction failed.")

4ba99402ef04f87c7978a576dd430379e77e7e72
Ferhat <47542683+FerhatYilmaz1986@users.noreply.github.com>
2019-12-06 11:28:18 -0500

    Feature : Insert more customer data

--- Database/sqlalchemy_test.py
+++ Database/sqlalchemy_test.py
@@ -92,3 +92,68 @@ def dispatch_order(order_id):
         print("Rolling back ...")
         session.rollback()
         print("Transaction failed.")
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+#Add customer data
+c1 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
+
+c2 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+
+session.add(c1)
+session.add(c2)
+session.new
+session.commit()
+c1.first_name, c1.last_name
+c2.first_name, c2.last_name
+c1.id
+c3 = Customer(
+    first_name="John",
+    last_name="Lara",
+    username="johnlara",
+    email="johnlara@mail.com",
+    address="3073 Derek Drive",
+    town="Norfolk"
+)
+
+c4 = Customer(
+    first_name="Sarah",
+    last_name="Tomlin",
+    username="sarahtomlin",
+    email="sarahtomlin@mail.com",
+    address="3572 Poplar Avenue",
+    town="Norfolk"
+)
+
+c5 = Customer(first_name='Toby',
+              last_name='Miller',
+              username='tmiller',
+              email='tmiller@example.com',
+              address='1662 Kinney Street',
+              town='Wolfden'
+              )
+
+c6 = Customer(first_name='Scott',
+              last_name='Harvey',
+              username='scottharvey',
+              email='scottharvey@example.com',
+              address='424 Patterson Street',
+              town='Beckinsdale'
+              )
+
+session.add_all([c3, c4, c5, c6])
+session.commit()
